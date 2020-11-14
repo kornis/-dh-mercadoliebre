3de3e79f2f6d54b4c0e2e86dc048f0e9d29e05e4
Federico Garcia <fedegarcia222@gmail.com>
2020-11-14 14:38:14 -0300

    arreglando vistas para acomodar elementos

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -11,17 +11,19 @@ const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 const controller = {
 	// Root - Show all products
 	index: (req, res) => {
-		return res.render('products');
+		// vuelvo a leero los archivos en index para no tener que actualizar la página una vez que modifico los datos en el json.
+		const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+		return res.render('products', { products });
 	},
 
 	// Detail - Detail from one product
 	detail: (req, res) => {
 		let id = req.params.id;
-		for(let i = 0; i < products.length; i++) {
-			console.log("id producto: " + products[i].id + " id parametro; "+ id)
+		for (let i = 0; i < products.length; i++) {
+			console.log("id producto: " + products[i].id + " id parametro; " + id)
 			if (products[i].id == id) {
 				return res.render('detail', { productoEncontrado: products[i] })
-			} 
+			}
 		}
 		return res.send("producto no encontrado");
 	},
@@ -47,17 +49,17 @@ const controller = {
 	// Update - Form to edit
 	edit: (req, res) => {
 		let id = req.params.id;
-		let productoEncontado = products.find(function(prod){
+		let productoEncontado = products.find(function (prod) {
 			return prod.id == id;
 		});
-		productoEncontado ? res.render('product-edit-form', {product: productoEncontado}) : res.send("Producto no encontrado");
+		productoEncontado ? res.render('product-edit-form', { product: productoEncontado }) : res.send("Producto no encontrado");
 	},
 	// Update - Method to update
 	update: (req, res) => {
 		let id = req.params.id;
 		let imagen = req.file;
-		let arrayModificado = products.map(function(prod){
-			if(prod.id == id){
+		let arrayModificado = products.map(function (prod) {
+			if (prod.id == id) {
 				prod = {
 					id,
 					...req.body,
@@ -73,7 +75,7 @@ const controller = {
 	// Delete - Delete one product from DB
 	destroy: (req, res) => {
 		let id = req.params.id;
-		let arrayFiltrado = products.filter(function(prod){
+		let arrayFiltrado = products.filter(function (prod) {
 			return prod.id != id;
 		});
 		fs.writeFileSync(productsFilePath, JSON.stringify(arrayFiltrado, null, 2));
